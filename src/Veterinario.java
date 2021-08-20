308725c0f04b6f181a648d58fd75e2561ad55203
BunnyMerz <tguerreirinhos.polo900@gmail.com>
2021-08-20 20:51:26 -0300

    Informações básicas do veterinário

--- src/Veterinario.java
+++ src/Veterinario.java
@@ -1,3 +1,11 @@
 public class Veterinario {
-    
+    String nome;
+    Int idade;
+    String cpf;
+
+    public Veterinario(String nome, Int idade, String cpf){
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
 }
