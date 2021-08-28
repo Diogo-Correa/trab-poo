43d269b58ae1d9e3d45b0b2b45580aa081cd5412
Diogo Corrêa <diogocorrea@id.uff.br>
2021-08-28 18:21:41 -0300

    Get's Enfermidade

--- src/models/util/Enfermidade.java
+++ src/models/util/Enfermidade.java
@@ -3,16 +3,20 @@ package models.util;
 // import java.util.ArrayList;
 
 public class Enfermidade {
-    /* private ArrayList<Procedimento> procedimento = new ArrayList<Procedimento>();
-        Ideia de implementacao.
-        Criar uma lista de procedimentos em Enfermidade e Veterinario
-        Identificar qual o veterinario mais eficaz para cada consulta.
-    */
+
     private String nome, gravidade;
 
     public Enfermidade(String nome, String gravidade) {
         this.nome = nome;
         this.gravidade = gravidade;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getGravidade() {
+        return this.gravidade;
     }
 
     
